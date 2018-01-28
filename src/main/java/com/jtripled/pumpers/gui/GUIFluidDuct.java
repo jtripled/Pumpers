5c45848f00bfe38911f61cd1d16345670fa9c114
jtripled <jtripled@gmail.com>
2018-01-28 17:03:06 -0600

    gui update

--- src/main/java/com/jtripled/pumpers/gui/GUIFluidDuct.java
+++ src/main/java/com/jtripled/pumpers/gui/GUIFluidDuct.java
@@ -1,10 +1,8 @@
 package com.jtripled.pumpers.gui;
 
-import com.jtripled.pumpers.Pumpers;
 import com.jtripled.pumpers.container.ContainerFluidDuct;
 import com.jtripled.voxen.gui.GUIContainerTile;
 import com.jtripled.voxen.gui.GUIFluidGauge;
-import net.minecraft.util.ResourceLocation;
 
 /**
  *
@@ -12,23 +10,15 @@ import net.minecraft.util.ResourceLocation;
  */
 public final class GUIFluidDuct extends GUIContainerTile<ContainerFluidDuct>
 {
-    public static final ResourceLocation TEXTURE = new ResourceLocation(Pumpers.ID, "textures/gui/tank.png");
-    
     public GUIFluidDuct(ContainerFluidDuct container)
     {
         super(container);
-        this.ySize = 132;
+        this.setType(Type.INVENTORY_1);
     }
     
     @Override
     public void addElements(int x, int y)
     {
         this.addElement(new GUIFluidGauge(this, container.getTile().getInternalTank(), 15, 22));
-    }
-
-    @Override
-    public ResourceLocation getTexture()
-    {
-        return TEXTURE;
     }
 }
--- src/main/java/com/jtripled/pumpers/gui/GUIPump.java
+++ src/main/java/com/jtripled/pumpers/gui/GUIPump.java
@@ -1,11 +1,10 @@
 package com.jtripled.pumpers.gui;
 
-import com.jtripled.pumpers.Pumpers;
 import com.jtripled.pumpers.container.ContainerPump;
 import com.jtripled.voxen.gui.GUIContainerTile;
 import com.jtripled.voxen.gui.GUIFluidGauge;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.fluids.FluidTank;
+import com.jtripled.voxen.gui.GUIInventorySlot;
+import com.jtripled.voxen.gui.GUIProgressIndicator;
 
 /**
  *
@@ -13,34 +12,23 @@ import net.minecraftforge.fluids.FluidTank;
  */
 public final class GUIPump extends GUIContainerTile<ContainerPump>
 {
-    public static final ResourceLocation TEXTURE = new ResourceLocation(Pumpers.ID, "textures/gui/pump.png");
-    
-    private final FluidTank tank;
-    
     public GUIPump(ContainerPump container)
     {
         super(container);
-        this.tank = container.getTile().getInternalTank();
-        this.ySize = 150;
+        this.setType(Type.INVENTORY_2);
     }
     
     @Override
     public void addElements(int x, int y)
     {
         this.addElement(new GUIFluidGauge(this, container.getTile().getInternalTank(), 15, 22));
-    }
-    
-    @Override
-    public void drawBackground(float ticks, int mouseX, int mouseY, int x, int y)
-    {
-        int progress = 22 - (int) Math.ceil(22 * (float) this.getContainer().getTile().getBucketCooldown() / 25);
-        drawTexturedModalRect(x + 77, y + 37, 176, 0, progress, 16);
-        drawTexturedModalRect(x + 17, y + 23, 0, 250, 141, 6);
-    }
-
-    @Override
-    public ResourceLocation getTexture()
-    {
-        return TEXTURE;
+        this.addElement(new GUIProgressIndicator(this, 77, 37) {
+            @Override
+            public float getProgress() {
+                return 1.0f - (float) container.getTile().getBucketCooldown() / 25.0f;
+            }
+        });
+        this.addElement(new GUIInventorySlot(this, 54, 35));
+        this.addElement(new GUIInventorySlot(this, 104, 35));
     }
 }
